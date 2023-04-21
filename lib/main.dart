6a5a8c6360722fad047a743c9737c2f3fcea9c68
5712labs <5712labs@gmail.com>
2023-04-21 15:06:49 +0900

    action

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
     ));
 
     return MaterialApp(
-        title: 'Flutter ChatAPIs',
+        title: 'Flutter ChatAPI',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
